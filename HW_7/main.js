bf7b30cb51aeea2689b39e7ae193c489588cf22c
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-14 15:50:39 +0300

    hw 7

---
+++ HW_7/main.js
@@ -0,0 +1,29 @@
+const name = 'Сергей';
+const age = 20;
+const role = 'User';
+const occupation = 'Design';
+
+console.log(
+    "1. !!name && (age > 18)  ======================== ",
+    !!name && (age > 18)
+);
+
+console.log(
+    "2. (role == 'Admin') || (occupation == 'CEO')  == ",
+    (role == 'Admin') || (occupation == 'CEO')
+);
+
+console.log(
+    "3. occupation ? occupation : role  ============== ",
+    occupation ? occupation : role
+);
+
+console.log(
+    "4. !!name && (age > 18) || role == 'Admin'  ===== ",
+    !!name && (age > 18) || role == 'Admin'
+);
+
+console.log(
+    "* (!!name && role !== 'Admin') && age  ========== ",
+    (!!name && role !== 'Admin') && age
+);
