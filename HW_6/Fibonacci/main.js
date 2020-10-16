5805d6415524ef66de341ba6d8097b8a4cbb3d24
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-17 02:29:28 +0300

    HW 6 Fibonacci

---
+++ HW_6/Fibonacci/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const index = +prompt('Введите порядковый номер числа из ряда Фибоначчи', '');
+
+alert(returnFibonacciNumber(index));
+
+function returnFibonacciNumber(number) {
+    let result = 0;
+    let lastResult = 1;
+    let preLastResult = 1;
+
+    if (number > 2) {   
+        for (let i = 2; i < number; i++) {
+            result = lastResult + preLastResult;
+            preLastResult = lastResult;
+            lastResult = result;
+        }
+    } else return 1;
+
+    return result;
+}
