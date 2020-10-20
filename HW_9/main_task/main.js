9bb69d015d320b0fa96cba752d9feae9761fc328
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-21 02:13:26 +0300

    hw_9...

--- HW_9/main_task/main.js
+++ HW_9/main_task/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let randomNumber = returnRandomNumber();
 let isGameStarted = true;
@@ -7,25 +7,35 @@ function returnRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 function runGame() {
-let i = 0
-  while (i < 10) {
-    let userNumber = prompt('Введите число от 1 до 100', '');
-    
-    if (!!userNumber || userNumber === null) return false;
-    
-    if (userNumber === randomNumber) {
-      winGame();
-    } else if (userNumber > randomNumber) {
-      console.log('Много');
-    } else console.log('Мало');
-    
-    console.log(randomNumber)
-    i++;
+  let tryCount = 1;
+  while (tryCount < 10) {
+    let userNumber = +prompt(
+      `Введите число от 1 до 100\nПопытка №${tryCount}`,
+      ""
+    );
+
+    if (!userNumber) return;
+
+    const isWin = compareNumbers(userNumber, randomNumber);
+
+    if (isWin) winGame();
+
+    console.log(randomNumber);
+    tryCount++;
   }
 }
 
+function compareNumbers(userNum, randNum) {
+  if (userNum === randNum) return true;
+  else if (userNum > randNum) console.log("Много");
+  else console.log("Мало");
+
+  return false;
+}
+
 function winGame() {
-  console.log('WIIN');
+  console.log("WIIN");
+  isGameStarted = false;
 }
 
 function startGame() {
