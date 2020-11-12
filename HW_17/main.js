be69e839014c4b44391df7750036a1bc23360a0b
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-11-12 11:17:34 +0200

    +

--- HW_17/main.js
+++ HW_17/main.js
@@ -1,4 +1,4 @@
 function random(min, max) {
-  const random = min + Math.random() * (max + 1 - min);
-  return Math.floor(random);
+  const randomFloat = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomFloat);
 }
