30b4695028f85b115010351105bbae3dd0c37d99
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-12-05 22:47:39 +0200

    HW_21 focus fix & in index

--- HW_21/main.js
+++ HW_21/main.js
@@ -1,23 +1,55 @@
 "use strict";
 
-const list = document.getElementById('list');
+const list = document.getElementById("list");
+const addButton = document.querySelector(".input__button_add");
+const removeButton = document.querySelector(".input__button_remove");
 
-list.addEventListener('click', activeListItems);
+list.addEventListener("click", activeListItems);
+addButton.addEventListener("click", addItemToList);
+removeButton.addEventListener("click", removeActiveItemFromList);
 
 let activeTarget = null;
 
 function activeListItems(e) {
+  const currTarget = e.target.closest(".list__item");
 
-  if (!activeTarget) activeTarget = e.target.closest('.list__item');
-  
-  if (!!activeTarget) activeTarget.classList.toggle('list__item_active');
-    
+  if (!currTarget) return;
 
-  if (!!activeTarget && !!e.target.closest('.input__button_remove')) {
-    
-    activeTarget.remove();
+  if (activeTarget === currTarget) {
+    blurActiveClass();
+
     activeTarget = null;
-    
-  }
+  } else {
+    blurActiveClass();
 
+    activeTarget = currTarget;
+
+    activeTarget.classList.add('list__item_active');
+  }
+}
+
+function blurActiveClass() {
+  document.querySelector('.list__item_active')?.classList.remove('list__item_active');
+}
+
+function addItemToList() {
+  const input = document.querySelector(".input__text");
+
+  if (input.value === "") return;
+
+  const listItemsHTML = list.innerHTML;
+  const newListItem = `<li class="list__item">${input.value}</li>`;
+
+  list.innerHTML = listItemsHTML + newListItem;
+
+  input.value = "";
+
+  blurActiveClass();
+}
+
+function removeActiveItemFromList() {
+  if (!activeTarget) return;
+
+  activeTarget.remove();
+  activeTarget = null;
 }
