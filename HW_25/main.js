9a5ee6458663632b3d210233ab01744aff74d8af
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-12-20 20:28:21 +0200

    25

--- HW_25/main.js
+++ HW_25/main.js
@@ -3,16 +3,19 @@ String.prototype.reverse = function () {
   return this.split("").reverse().join("");
 };
 
+
 // №2
 Array.prototype._sort = Array.prototype.sort;
 Array.prototype.sort = function (arg) {
   return [...this]._sort(arg);
 };
 
+
 // №3
 NodeList.prototype.map = Array.prototype.map;
 NodeList.prototype.reduce = Array.prototype.reduce;
 NodeList.prototype.find = Array.prototype.find;
+
 
 // *
 function immutableArray() {}
